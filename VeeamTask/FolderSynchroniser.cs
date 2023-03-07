35f694560da4e2a64231fa351adb8d4f5b659a37
Joshua <Joshua_lapine@hotmail.co.nz>
2023-03-07 21:30:25 +0100

    Initial commit. Periodic file copying implemented.

---
+++ VeeamTask/FolderSynchroniser.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Timers;
+
+namespace VeeamTask
+{
+    public class FolderSynchroniser
+    {
+        private DirectoryInfo sourceInfo;
+        private DirectoryInfo replicaInfo;
+        private int syncPeriodMinutes;
+
+        private static Timer syncTimer;
+
+        public FolderSynchroniser(string sourcePath, string replicaPath, string syncPeriodMinutes)
+        {
+            if (!Directory.Exists(sourcePath))
+            {
+                Console.WriteLine("Source folder directory not found");
+                Environment.Exit(3);
+            }
+
+            if (!Directory.Exists(replicaPath))
+            {
+                Console.WriteLine("Replica folder directory not found");
+                Environment.Exit(3);
+            }
+
+            sourceInfo = new DirectoryInfo(sourcePath);
+            replicaInfo = new DirectoryInfo(replicaPath);
+            this.syncPeriodMinutes = int.Parse(syncPeriodMinutes);
+
+            //Directory.Delete(Path.Combine(sourceInfo.FullName, "fold"), true);
+
+            SetupTimer();
+
+        }
+
+        private void SetupTimer()
+        {
+            syncTimer = new Timer(syncPeriodMinutes * 60 * 100); //Multiple of 6 seconds rather than minute
+            syncTimer.Elapsed += Sync;
+            syncTimer.AutoReset = true;
+            syncTimer.Enabled = true;
+        }
+
+        public void StartSync()
+        {
+            syncTimer.Start();
+        }
+
+        public void StopSync()
+        {
+            syncTimer.Stop();
+            syncTimer.Dispose();
+        }
+
+        private void Sync(Object source, ElapsedEventArgs e)
+        {
+            foreach (FileInfo tempFileInfo in sourceInfo.GetFiles())
+            {
+                tempFileInfo.CopyTo(Path.Combine(replicaInfo.FullName, tempFileInfo.Name), true);
+                //Console.WriteLine(Path.Combine(replicaInfo.FullName,tempFileInfo.Name));
+            }
+       
+        }
+
+        private void SyncFiles()
+        {
+            
+        }
+    }
+}
---
+++ VeeamTask/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Timers;
+
+namespace VeeamTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if(args.Length < 3)
+            {
+                Console.WriteLine("Invalid Arguments");
+                Environment.Exit(1);
+            }
+
+            FolderSynchroniser synchroniser = new FolderSynchroniser(args[0], args[1], args[2]);
+            synchroniser.StartSync();
+            Console.ReadLine();
+            synchroniser.StopSync();
+
+        }
+    }
+}
